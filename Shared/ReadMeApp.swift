9629ea61700f9e77ba378637ac8d4b5cc003e110
jeromeOthot <jerome.othot@gmail.com>
2021-11-22 09:42:17 -0500

    Initial Commit

---
+++ Shared/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  Shared
+//
+//  Created by admin on 2021-11-22.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
